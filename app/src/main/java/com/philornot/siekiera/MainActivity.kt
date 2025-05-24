0bdc3c6789e23d6a5aa851e47cebda374cec9a58
philornot <philornot@users.noreply.github.com>
2025-05-24 12:25:02 +0200

    Remove Sentry test exception
    The test exception, triggered on global layout, has been removed from `MainActivity.onCreate`.

--- app/src/main/java/com/philornot/siekiera/MainActivity.kt
+++ app/src/main/java/com/philornot/siekiera/MainActivity.kt
@@ -222,15 +222,6 @@ class MainActivity : ComponentActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // waiting for view to draw to better represent a captured error with a screenshot
-        findViewById<android.view.View>(android.R.id.content).viewTreeObserver.addOnGlobalLayoutListener {
-            try {
-                throw Exception("This app uses Sentry! :)")
-            } catch (e: Exception) {
-                Sentry.captureException(e)
-            }
-        }
-
         Timber.d("MainActivity onCreate")
 
         // Inicjalizacja AppConfig
